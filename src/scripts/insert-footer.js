0cba3bc7a50e6ed9b32e1975f5955599f2273fd1
DESKTOP-951NAKD\Lisa Ginter <lisa.ginter97@gmail.com>
2026-04-09 22:54:37 +0200

    add german footer

--- src/scripts/insert-footer.js
+++ src/scripts/insert-footer.js
@@ -1,29 +1,32 @@
-document.addEventListener('DOMContentLoaded', function() {
-    
-    const currentPath = window.location.pathname;
+document.addEventListener("DOMContentLoaded", function () {
+  const currentPath = window.location.pathname;
 
+  // Check if we are on the index page
+  let footerPath;
+  deOrEn = currentPath.includes("-de") ? "-de" : "";
+  if (
+    currentPath.endsWith("/") ||
+    currentPath.endsWith(".app") ||
+    currentPath.includes("index")
+  ) {
+    // On the index page, the footer path is relative to './src/pages'
+    footerPath = "./src/pages/footer" + deOrEn + ".html";
+  } else {
+    // On subpages, the footer path is relative to '../pages'
+    footerPath = "../pages/footer" + deOrEn + ".html";
+  }
 
-    // Check if we are on the index page
-    let footerPath;
-    if (currentPath.endsWith('/')||currentPath.endsWith('.app')||currentPath.includes('index')) {
-        // On the index page, the footer path is relative to './src/pages'
-        footerPath = './src/pages/footer.html';
-    } else {
-        // On subpages, the footer path is relative to '../pages'
-        footerPath = '../pages/footer.html';
-    }
+  // Function to load footer from an external file
+  function loadFooter() {
+    fetch(footerPath) // Path to footer.html (relative to the HTML page)
+      .then((response) => response.text())
+      .then((data) => {
+        // Insert footer content into the footer element
+        document.getElementById("sub-pages-footer").innerHTML += data;
+      })
+      .catch((error) => console.error("Error loading footer:", error));
+  }
 
-    // Function to load footer from an external file
-    function loadFooter() {
-        fetch(footerPath)  // Path to footer.html (relative to the HTML page)
-            .then(response => response.text())
-            .then(data => {
-                // Insert footer content into the footer element
-                document.getElementById('sub-pages-footer').innerHTML += data;
-            })
-            .catch(error => console.error('Error loading footer:', error));
-    }
-
-    // Load the footer once the DOM is ready
-    loadFooter();
+  // Load the footer once the DOM is ready
+  loadFooter();
 });
